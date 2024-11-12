38186fb2880354a4cd784ec9637b02ff45b80e8c
idahmed <idahmedyassine@gmail.com>
2024-11-12 11:53:15 +0100

    fix

--- tests/chapters/create_test.py
+++ tests/chapters/create_test.py
@@ -3,24 +3,6 @@ import pytest
 import os
 BASE_URL = os.getenv("BASE_URL")
 
-
-@pytest.fixture(scope="session")
-def playwright():
-    with sync_playwright() as p:
-        yield p
-
-@pytest.fixture
-def browser(playwright):
-    browser = playwright.chromium.launch(headless=True)
-    yield browser
-    browser.close()
-
-@pytest.fixture
-def page(browser):
-    page = browser.new_page()
-    yield page
-    page.close()
-
 def test_chapters_create(page):
     page.goto(BASE_URL + "/chapters/create/")
     assert page.locator('h2:has-text("Chapters")').is_visible()
