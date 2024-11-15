5b1cde4ca36c099816871dfaf1e24963ace20176
idahmed <idahmedyassine@gmail.com>
2024-11-15 10:22:33 +0100

    required field

--- tests/chapters/chapter_create_test.py
+++ tests/chapters/chapter_create_test.py
@@ -13,17 +13,38 @@ def test_chapter_create_page(page):
     page.fill("#password", "admin")
     page.click("button:has-text('Login')")
     assert page.url == f"{BASE_URL}"
-    page.goto(BASE_URL + "chapters")
-    assert page.url == f"{BASE_URL}chapters/"
-    add_button = page.locator("a.btn.btn-primary:has-text('Add')")
-    assert add_button.is_visible()
-    assert add_button.get_attribute("href") == "/chapters/create/"
-    add_button.click()
-    assert page.url == f"{BASE_URL}chapters/create/"
-    page.fill("#code", "Test Chapter")
-    page.fill("#name", "Test Chapter")
-    page.click("button:has-text('Save')")
-    assert page.url == f"{BASE_URL}chapters/"
-    assert page.locator("div.alert.alert-success").is_visible()
+    page.goto(BASE_URL + "chapters/create")
+    code_is_required = page.locator('#code').get_attribute('required')
+    assert code_is_required is not None
+
+    comment_is_required = page.locator('#comment_placement').get_attribute('required')
+    assert comment_is_required is None
+
+    
+
+
+
+
+
+
+    # assert page.url == f"{BASE_URL}chapters/"
+    # add_button = page.locator("a.btn.btn-primary:has-text('Add')")
+    # page.screenshot('fullpage-screenshot.png');
+    # assert add_button.is_visible()
+    # assert add_button.get_attribute("href") == "/chapters/create/"
+    # add_button.click()
+    # assert page.url == f"{BASE_URL}chapters/create/"
+    # page.fill("#code", "Test Chapter")
+
+    # page.click("button:has-text('Save')")
+
+    # assert add_button.get_attribute("href") == "/chapters/create/"
+
+
+    # page.fill("#name", "Test Chapter")
+    # assert page.url == f"{BASE_URL}chapters/"
+    # assert page.locator("div.alert.alert-success").is_visible()
+
+    # page.goto
     
     
